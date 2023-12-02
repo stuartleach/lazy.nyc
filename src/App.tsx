cde41000eb129acef2884d12dfef1cc05334c21b
Stuart Leach <jstuartleach@gmail.com>
2023-12-02 15:13:35 -0500

    new

--- src/App.tsx
+++ src/App.tsx
@@ -8,8 +8,9 @@ function App() {
     return (
         <div className="App">
             <header className="App-header">
-                <img src={Logo} width={"50%"} color={"white"} alt="logo"/>
-
+                <img src={Logo} width={"50%"} style={{maxWidth: "80%", maxHeight: "80%"}} color={"white"} alt="logo"/>
+                <input type="text" placeholder="fuck you"
+                       className="bg-gray-200 rounded-full px-4 py-2 focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent"/>
                 {/*<h1 className="font-black text-6xl font-serif mt-6">*/}
                 {/*    Coming soon.*/}
                 {/*</h1>*/}
